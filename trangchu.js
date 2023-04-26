52a927f00f0cafece1cb1b7e0b97033d465159b2
P-V-Hung <azhung08102004@gmail.com>
2023-04-26 12:58:54 +0700

    first commit

---
+++ trangchu.js
@@ -0,0 +1,12 @@
+alert('Xin chào, Đây là trang quản lý khóa học')
+// let banner = document.getElementById('banner');
+// console.log(banner)
+// let arrBanner = ['img/banner1.png', 'img/banner2.png', 'img/banner3.jpg'];
+// let i = 0;
+// let _next = () => {
+//     i++;
+//     if (i > 2) {
+//         i = 0;
+//     }
+//     banner.src = arrBanner[i];
+// } 
